fc8fb312014fb838e460027f2ca0748069cc6b76
littleconan@hanmail.net <littleconan@hanmail.net>
2025-05-28 10:41:34 +0900

    0528

--- mysql연동/커넥션풀1.py
+++ mysql연동/커넥션풀1.py
@@ -1,3 +1,4 @@
+#conda activate myenv1 
 #pip install sqlalchemy 
 #버전 2.0이상 
 #C:\Users\littl\AppData\Roaming\Python\Python38\site-packages\sqlalchemy
@@ -15,7 +16,7 @@ engine = create_engine(
 )
 
 try:
-    conn = engine.connect()
+    conn = engine.connect() #연결객체를 얻는다 
     print("데이터베이스 연결 성공")
 except SQLAlchemyError as e:
     print("데이터베이스 연결 실패:", e)
@@ -23,6 +24,7 @@ except SQLAlchemyError as e:
 
 #2.0 이전 버전 conn.execute("쿼리")
 result = conn.execute(text("SELECT * FROM emp"))
+#tuple로 출력한다 
 # for row in result:
 #     print(row)
 
@@ -32,7 +34,7 @@ for row in rows:
         print(dict(row))
 conn.close()
 
-#데이터추가하기
+#데이터추가하기 - 파라미터 처리방식
 conn = engine.connect() 
 sql = text("""
     insert into emp (empno, ename, sal )
@@ -40,9 +42,27 @@ sql = text("""
 """)
 conn.execute( sql, [{"empno":10001, 
                      "ename":"우즈2", 
-                     "sal":8000}, 
-                     {"empno":10002, 
-                     "ename":"우즈3", 
                      "sal":8000}] )
 conn.commit() 
 conn.close() 
+
+"""
+orm - object - relation - mapping
+      객체지향언어 - 관계형디비- 연결하는 프로그램 기법 
+      django(장고, 파이썬 웹프레임워크),
+      jpa - java 
+      파이썬에서 SqlAlchemy 가 지원한다 
+      전체테이블이 10개 미만일때 
+      테이블 3개이상 조인시 속도문제, 테이블간의 구조가 복잡해질
+      수록 orm만으로는 처리가 되지 않는다  
+SPA - single page application, 인스타그램, 페이스북 등의
+    개발기법, 웹페이지를 하나만 만들어서 페이지 체인지를 
+    하는방식으로 화면이동이 훨씬 매끄럽다 무한스크롤등을 지원한다
+    react(압도적), vuejs(우리나라 금융권), angular, polymer(유투부)   
+    인스타그램 개발하다가 만들은게 react라이브러리 
+    spa 방식 웹개발시 orm방식을 많이 사용한다  
+
+sqlite - 본래의미의 디비는 아니고 사실은 파일이고 
+        다만 쿼리형식으로 읽고 쓰기는 가능하나 
+        네트워크가 안됨 , 휴대폰의 전화번호부 
+"""
---
+++ mysql연동/커넥션풀3.py
@@ -0,0 +1,59 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+
+# SQLAlchemy가 PyMySQL을 내부적으로 사용하며 pool 지원
+engine = create_engine(
+    "mysql+pymysql://root:1234@localhost:3306/mydb",
+    pool_size=10,         # 최대 연결 수
+    max_overflow=5,       # 초과 시 추가 연결 수
+    pool_recycle=3600     # 재활용 시간
+)
+
+#1. 데이터가져오기
+with engine.connect() as conn:
+    sql = """
+    select empno, ename, sal 
+    from emp 
+    """
+    result = conn.execute(text(sql))
+    for row in result.all():
+        print(row)
+    
+    #dicttype으로
+    result = conn.execute(text(sql))
+    rows = result.mappings().all()
+    for row in rows:
+        print(dict(row))
+
+#2.검색어를 전달할때 
+ename = "조승연32" #키보드입력으로 변경 
+
+with engine.connect() as conn:
+    sql = """
+        select empno, ename, sal 
+        from emp 
+        where ename=:name 
+    """
+    # :name
+    result = conn.execute(text(sql), [{"name":ename}])
+    temp = result.all()
+    if len(temp) == 0:
+        print("없음")
+    else:
+        for row in temp:
+            print(row) 
+
+
+#3.insert 
+with engine.connect() as conn:
+    sql = """
+        select ifnull(max(empno), 0)+1 
+        from emp 
+    """
+    result = conn.execute(sql) #[()]
+    empno = result.all()[0][0]
+    sql = """
+        insert into emp(empno, ename, sal)
+        values(:empno, :ename, :sal)
+    """
+    conn.execute(text(sql), [{"empno":empno, "ename":""}])
