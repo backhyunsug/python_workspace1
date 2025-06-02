2f5d227b7256281ebf1ad979c26b5190949a2b80
littleconan@hanmail.net <littleconan@hanmail.net>
2025-06-02 11:48:55 +0900

    0602

---
+++ 자료구조/펠린드롬.py
@@ -0,0 +1,12 @@
+word = "racecar"
+if word == word[::-1]:
+    print(True)
+else:
+    print(False)
+
+#절반을 쪼개서 - 길이 
+#길이 ln len(문자열길이)-1 
+#0        ln-0          arr[0] <-> arr[ln-0] 
+#1        ln-1          arr[1] <-> arr[ln-1]
+#2        ln-2          arr[2] <-> arr[ln-2]
+#3        ln-3          arr[3] <-> arr[ln-3]
