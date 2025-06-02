d4fe9be2664f7809256e30f96920243678f254db
littleconan@hanmail.net <littleconan@hanmail.net>
2025-06-02 13:26:57 +0900

    0602

--- 자료구조/펠린드롬.py
+++ 자료구조/펠린드롬.py
@@ -10,3 +10,18 @@ else:
 #1        ln-1          arr[1] <-> arr[ln-1]
 #2        ln-2          arr[2] <-> arr[ln-2]
 #3        ln-3          arr[3] <-> arr[ln-3]
+
+import math
+def palindrom(s):
+    ln = len(s)-1 
+    #6   6/2 - 3 
+    for i in range(0, math.ceil(ln/2)):
+        if s[i] != s[ln-i]:
+            return False 
+    
+    #마지막까지 남았다는 말은 회문이 성립한다
+    return True 
+
+print( palindrom("madam") )
+print( palindrom("tomato") )
+print( palindrom("abba") )
