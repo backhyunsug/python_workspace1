26a50379a49914710ae10ebcb331b154a5952552
littleconan@hanmail.net <littleconan@hanmail.net>
2025-06-04 13:32:19 +0900

    0604

--- 자료구조/번호뽑기.py
+++ 자료구조/번호뽑기.py
@@ -63,7 +63,7 @@ class MyQueue:
             size=10 
         self.size = size 
         self.queue = [None] * self.size
-        print(self.queue)
+        #print(self.queue)
         self.front = 0 
         self.rear = 0
     
@@ -118,34 +118,44 @@ class MyQueue:
             #print("front", self.front, "rear", self.rear, temp)
         
 
-
-if __name__ == "__main__":
-    q = MyQueue()
-    q.put('A')
-    q.put('B')
-    q.put('C')
-    q.put('D')
-    q.put('E')
-    q.put('F')
-    q.put('G')
-    q.put('H')
-    q.put('I')
-    q.put('J')
-    q.get()
-    q.get()
-    q.get() 
-    q.put('J')
-    q.put('K')
-    
-    q.print() 
-    
-    # while not q.isEmpty():
-    #     print( q.get())
-
 """
 은행에 가면 순번뽑기 
 1.은행원 - 작업완료 -> 몇번 손님 나오세요
 2.고객 - 번호뽑기 -> 대기인원 
 """
 class BankNumber:
-    pass 
+    def __init__(self):
+        self.number=0 #번호가 0 ~~~~~  
+        self.cnt=0    #현재 대기 인원수 
+        self.queue = MyQueue(500) #큐의 크기 500 
+
+    def menuCustomer(self):
+        self.number += 1
+        self.cnt+=1 
+        self.queue.put(self.number)
+        print(f"대기인원 {self.cnt}" )
+        print(f"고객님 번호는 {self.number} 입니다")
+    
+    def menuBanker(self):
+        #큐에서 번호 하나를 꺼낸다 
+        if self.queue.isEmpty():
+            return 
+        number = self.queue.get() 
+        print(f"{number}번 고객님 창구앞으로 와주세요")
+        self.cnt -= 1
+        print(f"대기인원 {self.cnt}" )
+
+    def main(self):
+        while True:
+            sel = input("1.고객 2.은행원 3.종료")
+            if sel=="1":
+                self.menuCustomer()
+            elif sel=="2":
+                self.menuBanker()
+            elif sel=="3":
+                return 
+            else: print("쫌")
+
+if __name__ == "__main__":
+   bm = BankNumber()
+   bm.main()
