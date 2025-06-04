93910ebe90f5405a441c2bab2eb37b29ecb9ad60
littleconan@hanmail.net <littleconan@hanmail.net>
2025-06-04 11:08:14 +0900

    0604

--- 자료구조/큐1.py
+++ 자료구조/큐1.py
@@ -58,7 +58,7 @@
  
 """
 class MyQueue:
-    def __init__(self, size):
+    def __init__(self, size=10):
         if size<10: #최소 10이상
             size=10 
         self.size = size 
@@ -66,4 +66,71 @@ class MyQueue:
         print(self.queue)
         self.front = 0 
         self.rear = 0
+    
+    def put(self, data):
+        """
+        rear 하나 증가시킨다음에 % self.size 로 나머지 구하고 
+        그 위치에 데이터 넣기 
+        """
+        if self.isFull():
+            print("queue is full")
+            return 
+        self.rear = (self.rear+1)%self.size
+        self.queue[self.rear]=data
 
+    def isEmpty(self):
+        if self.rear==self.front:
+            return True 
+        return False 
+    
+    def isFull(self):
+        if (self.rear+1)%self.size==self.front:
+            return True 
+        return False 
+
+    def get(self): 
+        """
+        front 증가시켜서 그 위치값 반화하기     
+        """
+        if self.isEmpty():
+            print("queue is empty")
+            return None
+        self.front = (self.front+1)%self.size 
+        return self.queue[self.front]
+    
+    def peek(self):
+        """
+        front 증가시켜서 그 위치값 반화하기
+        front자체는 바꾸면 안된다
+        """
+        if self.isEmpty():
+            print("queue is empty")
+            return None
+        temp = (self.front+1)%self.size 
+        return self.queue[temp]
+    
+if __name__ == "__main__":
+    q = MyQueue()
+    q.put('A')
+    q.put('B')
+    q.put('C')
+    q.put('D')
+    q.put('E')
+    q.put('F')
+    q.put('G')
+    q.put('H')
+    q.put('I')
+    q.put('J')
+    
+    print( q.queue)
+    print(q.peek())
+    print( q.queue)
+    
+    while not q.isEmpty():
+        print( q.get())
+
+"""
+은행에 가면 순번뽑기 
+1.은행원 - 작업완료 -> 몇번 손님 나오세요
+2.고객 - 번호뽑기
+"""
