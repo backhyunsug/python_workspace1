b7fb0cf78064c3f17f328ba9761d622f64aa6e06
littleconan@hanmail.net <littleconan@hanmail.net>
2025-06-05 17:20:41 +0900

    0605

--- 자료구조/괄호짝맞추기.py
+++ 자료구조/괄호짝맞추기.py
@@ -41,4 +41,47 @@ print( isMatch(s) )
                          14 2.3333...
                          14 - 2.3333  11.6666
                          11.666666666                             
+
+피연산자(숫자)는 스택에 넣는다. 그럼, 스택의 상태는 [3, 5, 2]이다.
+연산자를 만나면 스택에서 피연산자 두 개를 꺼내서 계산한다.
+연산자 *를 만났으므로 스택에서 2와 5를 꺼내서 곱한다. 5 * 2 = 10
+결괏값을 스택에 저장한다. 이제 스택의 상태는 [3, 10]다.
+그다음에 연산자 +가 있으므로 스택에서 10과 3을 꺼내서 더한다. 답은 13이다.                         
 """
+
+s = "4 5 2 * + 7 3 / -"
+#한글자 받아가서 숫자인지 확인하는 함수 
+def isDigit(ch):
+    if(ord(ch)>=ord('0') and ord(ch)<=ord('9')):
+        return True 
+    return False 
+
+def isOperator(ch):
+    if ch=="+" or ch=="-" or ch=="*" or ch=="/":
+        return True 
+    return False 
+
+def operate(c, left, right):
+    if c =="+": 
+        result = int(left) + int(right)
+    elif c=="-":
+        result = int(left) - int(right)
+    elif c=="*":
+        result = int(left) * int(right)
+    else:
+        result = int(left) // int(right)  
+    return result 
+
+def getResult( postfix): #수식을 받아가서 연산 결과를 반환해야 한다 
+    stack = MyStack(100)
+    for c in postfix: 
+        if isDigit(c):
+            stack.push(c)
+        elif isOperator(c):
+            right = stack.pop()
+            left = stack.pop() 
+            result = operate(c, left, right)
+            stack.push(result)
+    return stack.pop()
+
+print( getResult(s))
