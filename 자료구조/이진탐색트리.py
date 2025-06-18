63be660574f11cffa09fe50d7eee810cae75a9e0
littleconan@hanmail.net <littleconan@hanmail.net>
2025-06-18 14:49:26 +0900

    9018

--- 자료구조/이진탐색트리.py
+++ 자료구조/이진탐색트리.py
@@ -53,6 +53,94 @@ class BinarySearchTree:
         else: 
             parent.right = newNode
 
+    def inorder(self, node):
+        if node==None:
+            return 
+        self.inorder(node.left)
+        print(node.data.num)
+        self.inorder(node.right)
+
+    #데이터 검색함수 
+    def search(self, key):
+        current = self.root #검색시작위치 
+        count = 0 #찾은 회수 
+        while current:
+            if key.num == current.data.num: #찾았으니까 
+                return count 
+            count+=1 
+            if key.num < current.data.num:
+                current = current.left 
+            else:
+                current = current.right 
+
+        return -1
+
+
+    def find(self, key):
+        parent = None       # 삭제될 노드의 부모와 자식을 연결해야 한다 
+        current = self.root # 삭제될 노드의 위치를 찾는다 
+        find = False #못찾을 
+        while current and not find:
+            if current.data.num == key.num:
+                find = True 
+            else:
+                parent = current 
+                if key.num < current.data.num: 
+                    current = current.left 
+                else:
+                    current = current.right 
+
+        return find, parent, current 
+                
+    def delete(self, key):
+        #삭제하려고 할 경우에 삭제될 노드를 찾아야 한다. 
+        if self.root == None:
+            return 
+        found, parent, current = self.find(key) 
+        if found == False: #삭제 대상이 없다. 
+            return         
+        
+        #1. 삭제대상이 자식이 없는 경우 - 그냥 나를 삭제하면 된다. 
+        if current.left==None and current.right==None:
+            if parent.left ==current: #내가 부모 노드의 왼쪽에 있었다면 
+                parent.left=None 
+            else:                     #내가 부모의 오른쪽에 있었다면 
+                parent.right=None 
+            return 
+        
+        if current.left != None or current.right!= None:
+            if current.left != None: #왼쪽에 자식이 있으면 그 자식을 가져온다
+                current.data = current.left.data
+            else: #오른쪽에 자식이 있음 
+                current.data = current.right.data
+            return 
+        
+
+        #자식이 둘다 있을때 트리 전체를 재편한다 
+        #삭제될 대상의 오른쪽 서브트리에서 가장 작은 대상을 
+        #찾아 바꿔치기를 한다 
+        #탐색을 다시 해야 한다 
+        
+
+
+
+
+
+
+
+if __name__=="__main__":
+    bst = BinarySearchTree()
+    arr = [16, 8, 9, 2, 4, 12,17, 21, 23]
+    for i in arr:
+        bst.insert(Data(i))
+    bst.inorder(bst.root)
+
+    print(bst.search(Data(16)))
+    print(bst.search(Data(2)))
+    print(bst.search(Data(26)))
+
+
+
 
 
 
