1a74eb0d21ea7f99f880a12b19d1a8603236658d
littleconan@hanmail.net <littleconan@hanmail.net>
2025-07-18 11:02:28 +0900

    0718

--- 딥러닝/꽃분류2.py
+++ 딥러닝/꽃분류2.py
@@ -17,10 +17,14 @@ def makeData(flower_name, label, isTrain=True):  #train/daisy 0     train/dandel
     data = []
     labels = [] 
     #print( os.listdir(path)) #해당 경로에 파일명을 모두 가져온다 
-    #파일 하나씩 읽어서 넘파이배열로 만들어서 data 에 추가시키기 
+    #파일 하나씩 읽어서 넘파이배열로 만들어서 data 에 추가시키기
+    i=1 
     for filename in os.listdir(path):
         try:
-            print(filename + "....") 
+            if i%100==0:
+                print(f"{i} 번째파일 처리중 .... ")
+            i+=1
+
             #파일 속성도 확인해보자 
             kind = imghdr.what(path+"/"+filename)
             if kind in ["gif", "png", "jpeg", "jpg"]: #이미지일때만 
@@ -38,11 +42,95 @@ def makeData(flower_name, label, isTrain=True):  #train/daisy 0     train/dandel
     if not isTrain:
         title = "test"
     savefileName = "imagedata{}.npz".format(str(label)+'_'+title)
-    np.savez(savefileName, data=data, targets=label)
+    np.savez(savefileName, data=data, targets=labels)
+
+def initData():
+    flowers = ["daisy", "dandelion"]
+    i=0
+    for f in flowers:
+        makeData(f, i, True) 
+        makeData(f, i, False) 
+        i+=1 
+
+def loadData():
+    f1 = np.load("imagedata0_train.npz")
+    f2 = np.load("imagedata1_train.npz")
+    d1 = f1["data"]
+    t1 = f1["targets"]
+    d2 = f2["data"]
+    t2 = f2["targets"]
+    #print(d1[:10])
+    print(t1[:10])
+    
+    X_train = np.concatenate( (d1, d2), axis=0)
+    y_train = np.concatenate( (t1, t2), axis=0)
+
+    f1 = np.load("imagedata0_test.npz")
+    f2 = np.load("imagedata1_test.npz")
+    d1 = f1["data"]
+    t1 = f1["targets"]
+    d2 = f2["data"]
+    t2 = f2["targets"]
+
+    X_test = np.concatenate( (d1, d2), axis=0)
+    y_test = np.concatenate( (t1, t2), axis=0)
+
+    return X_train, y_train, X_test, y_test 
+    
+from tensorflow.keras import models, layers 
+
+def createModel():
+    network = models.Sequential(
+        [
+            layers.Dense(128, activation="relu"),
+            layers.Dense(64,  activation="relu"),
+            layers.Dense(64,  activation="relu"),
+            layers.Dense(32,  activation="relu"),
+            layers.Dense(2,   activation="softmax") 
+        ]
+    )
+
+    network.compile( optimizer='rmsprop', 
+                     loss='sparse_categorical_crossentropy',
+                     metrics=['accuracy'])
+    return network 
+
+from sklearn.preprocessing import StandardScaler
+
+def preprocessing():
+    X_train, y_train, X_test, y_test = loadData()
+    #차원이   현재 4차원 => 2차원으로 
+    X_train = X_train.reshape(X_train.shape[0],  80*80*3 )
+    X_test  = X_test.reshape(X_test.shape[0],  80*80*3 )
+    scaler = StandardScaler() 
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.fit_transform(X_test)
+
+    return X_train_scaled, y_train, X_test_scaled, y_test 
+
+def main():
+    X_train, y_train, X_test, y_test = preprocessing() 
+    network = createModel() 
+    network.fit(X_train, y_train, epochs=100, batch_size=100)
+
+    train_loss, train_acc  = network.evaluate(X_train, y_train)
+    test_loss, test_acc  = network.evaluate(X_test, y_test)
+    print(f"훈련셋   손실값 :{train_loss}  정확도:{train_acc}")
+    print(f"테스트셋 손실값 :{test_loss}  정확도:{test_acc}")
 
 if __name__ == "__main__":
-    makeData("daisy", 0, True) 
-    # makeData("daisy", 0, False) 
-    # makeData("dandelion", 0, True) 
-    # makeData("dandelion", 0, True) 
+    main()
     
+    #initData()
+    #X_train, y_train, X_test, y_test = loadData()
+    # print(X_train.shape)
+    # print(X_train[:5])
+    # print(y_train.shape)
+    # print(y_train[:5])
+    # print(X_test.shape)
+    # print(X_test[:5])
+    # print(y_test.shape)
+    # print(y_test[:5])
+    
+        
+
