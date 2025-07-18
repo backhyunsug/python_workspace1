7127d459cec44828b49854c6c31f0212635d598f
littleconan@hanmail.net <littleconan@hanmail.net>
2025-07-18 15:40:07 +0900

    0718

---
+++ 딥러닝/쓰레기분류.py
@@ -0,0 +1,130 @@
+import numpy as np 
+import os 
+import random 
+import PIL.Image as pilimg 
+import imghdr
+import pandas as pd 
+import tensorflow as tf 
+from sklearn.model_selection import train_test_split
+
+#데이터만들기 folder 를 읽어서 데이터를 만들어보자 - train 폴더 
+base_path = "./data/Garbageclassification"
+title = "thresh"
+
+def makeData(flower_name, label, isTrain=True):  #train/daisy 0     train/dandelion 1 
+    if isTrain:
+        path = base_path + "/"+ flower_name 
+    else:
+        path = base_path + "/"+ flower_name 
+    #print(path)
+    data = []
+    labels = [] 
+    #print( os.listdir(path)) #해당 경로에 파일명을 모두 가져온다 
+    #파일 하나씩 읽어서 넘파이배열로 만들어서 data 에 추가시키기
+    i=1 
+    for filename in os.listdir(path):
+        try:
+            if i%100==0:
+                print(f"{i} 번째파일 처리중 .... ")
+            i+=1
+
+            #파일 속성도 확인해보자 
+            kind = imghdr.what(path+"/"+filename)
+            if kind in ["gif", "png", "jpeg", "jpg"]: #이미지일때만 
+                img = pilimg.open(path+"/"+filename)  #파일을 읽어서 numpy 배열로 바꾼다 
+                resize_img = img.resize((80,80)) #사이즈는 특성이 너무 많으면 계산시간도 오래걸리고 
+                                                 #크기가 각각이면 학습불가능 그래서 크기를 맞춘다 
+                pixel = np.array(resize_img)
+                if pixel.shape == (80,80,3):
+                    data.append(pixel)
+                    labels.append(label)    
+        except:
+            print(filename + " error")       
+
+
+    savefileName = "imagedata{}.npz".format(str(label)+'_'+title)
+    np.savez(savefileName, data=data, targets=labels)
+
+def initData():
+    #print(os.listdir(base_path))
+    foldername = os.listdir(base_path)
+    i=0
+    for f in foldername:
+        makeData(f, i, True) 
+        i+=1 
+    print("데이터 저장 완료")
+
+def loadData():
+    foldername = os.listdir(base_path)
+    dataList =[]
+    targetList =[]
+    for i in  range(0, len(foldername)):
+        f1 = np.load(f"imagedata{i}_thresh.npz")
+        dataList.append(f1["data"])
+        targetList.append(f1["targets"])
+            
+    X = np.concatenate( tuple(dataList), axis=0)
+    y = np.concatenate( tuple(targetList), axis=0)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=10, test_size=0.3) 
+    print(X_train.shape, y_train.shape)
+    print(X_test.shape, y_test.shape)
+    
+    return X_train, y_train, X_test, y_test
+    
+from tensorflow.keras import models, layers 
+
+def createModel():
+    network = models.Sequential(
+        [
+            layers.Dense(128, activation="relu"),
+            layers.Dense(64,  activation="relu"),
+            layers.Dense(32,  activation="relu"),
+            layers.Dense(6,   activation="softmax") 
+        ]
+    )
+
+    network.compile( optimizer='rmsprop', 
+                     loss='sparse_categorical_crossentropy',
+                     metrics=['accuracy'])
+    return network 
+
+from sklearn.preprocessing import StandardScaler
+
+def preprocessing():
+    X_train, y_train, X_test, y_test = loadData()
+    #차원이   현재 4차원 => 2차원으로 
+    X_train = X_train.reshape(X_train.shape[0],  80*80*3 )
+    X_test  = X_test.reshape(X_test.shape[0],  80*80*3 )
+    scaler = StandardScaler() 
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.fit_transform(X_test)
+
+    return X_train_scaled, y_train, X_test_scaled, y_test 
+
+def main():
+    X_train, y_train, X_test, y_test = preprocessing() 
+    network = createModel() 
+    network.fit(X_train, y_train, epochs=20, batch_size=100)
+
+    train_loss, train_acc  = network.evaluate(X_train, y_train)
+    test_loss, test_acc  = network.evaluate(X_test, y_test)
+    print(f"훈련셋   손실값 :{train_loss}  정확도:{train_acc}")
+    print(f"테스트셋 손실값 :{test_loss}  정확도:{test_acc}")
+
+if __name__ == "__main__":
+    main()
+    
+    #initData()
+    #X_train, y_train = loadData()
+    #print(X_train.shape)
+    #print(X_train[:5])
+    #print(y_train.shape)
+    #print(y_train[:5])
+    # print(X_test.shape)
+    # print(X_test[:5])
+    # print(y_test.shape)
+    # print(y_test[:5])
+    
+        
+
