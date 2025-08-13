42a917864f94fc385be258481d24ef2d4a70edfb
bhs <littleconan@hanmail.net>
2025-08-13 11:40:25 +0900

    Add database manager and board list endpoint
    Introduced a Database class for managing MySQL connections and queries. Updated board router to use the new Database class and added a '/list' endpoint to fetch all board entries from 'tb_board'.

--- 프론트엔드/fastapi/fastapi_workspace/backend/routers/board.py
+++ 프론트엔드/fastapi/fastapi_workspace/backend/routers/board.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse 
 
 from fastapi import APIRouter, Depends 
+from database import Database
 
 router = APIRouter(
     prefix="/board",  #url요청이 /board/~~~~ 로 오는것은 여기서 다 처리한다는 의미임 
@@ -12,4 +13,12 @@ router = APIRouter(
 @router.get("/")
 def board_index():
     return {"msg":"게시판입니다"}
+
+@router.get("/list")
+def board_list():
+    with Database() as db_mgr:
+        sql = "select * from tb_board" 
+        results = db_mgr.executeAll(sql)
+    return {"list":results}
+
 
